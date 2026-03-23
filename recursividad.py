35df383d1dd7f21b5d5e17f2158d2f0ea1a5f43f
ESEPAPU-UWU <2510146@cesba-queretaro.edu.mx>
2026-03-23 08:12:16 -0600

    Segundo comit nuevos cambios

--- recursividad.py
+++
@@ -1,8 +0,0 @@
-def cuenta_regresiva(n):
-    if n == 0:  # caso base
-        print("Fin")
-    else:       # caso recursivo
-        print(n)
-        cuenta_regresiva(n - 1)
-
-cuenta_regresiva(5)
